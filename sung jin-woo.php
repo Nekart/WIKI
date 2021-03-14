1310e288356fb08f6160074099fad5a4826f1061
nekart <nekart972.mrg@gmail.com>
2021-03-13 21:25:15 -0400

    firts commit

---
+++ sung jin-woo.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>SiteWikiNekart</title>
+    <link rel="stylesheet" href="style.css" />
+    <script type="text/javascript" src="script.js"></script>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul id="navbar">
+
+          <li><a href="index.html">Acceuil</a></li>
+          <li><a href="sung jin-woo.php">Sung Jin-Woo</a></li>
+          <li><a href="#">Aide</a></li>
+        </ul>
+      </nav>
+    </header>
+    <div id="corp" class="corpx">
+    <section  id="corp" class="corp" >
+      <div>
+      <h1>Sung Jin-Woo</h1>
+       <p>
+          Sung Jin-Woo (성진우成 秦 禹) est le protagoniste de Solo Leveling .
+          Il était auparavant connu comme le chasseur le plus faible du monde et
+          a exploré la voie pour devenir le chasseur le plus fort du monde après
+          avoir été réveillé et devenu joueur . Jin-Woo a été réveillé en tant
+          que joueur après avoir survécu au temple du Cartenon et terminé la
+          quête secrète " Courage of the Weak ". Le mystérieux système a permis
+          à Jin-Woo de monter de niveau sans limites et d'augmenter sa force,
+          contrairement à tous les chasseurs, qui sont incapables d'augmenter
+          leur force à moins d'être réveillé. Attention! Ce qui suit sont de
+          nouveaux spoilers légers qui n'ont pas été couverts dans le Solo
+          Leveling Manhwa. Il est le fondateur et maître de guilde de la guilde
+          Ahjin qu'il a créée aux côtés de Yoo Jin-Ho pour lui permettre de
+          nettoyer les donjons en utilisant ses ombres .
+        </p>
+        <h2 id="a">Apparence </h2>
+        <p>
+          Avant son réveil, Jin-Woo était un jeune homme de taille moyenne avec
+          de longs cheveux noirs et des yeux gris; sa carrure était fragile, et
+          bien qu'il soit considéré comme plus fort qu'un humain ordinaire, il
+          était le plus faible parmi les chasseurs. Avant l'incident du donjon
+          de rang D, il portait un sweat à capuche gris et un maillot de corps
+          marron aux côtés d'un jean bleu. Après son réveil en tant que joueur ,
+          Jin-Woo a pu devenir plus fort en augmentant ses statistiques, ce qui
+          a conduit à un changement soudain de toute sa stature / apparence. À
+          cause de cela, il a grandi de quelques centimètres; ses traits du
+          visage sont devenus beaucoup plus nets et il a acquis une construction
+          maigre / musclée. Son comportement tout entier est également devenu
+          plus sérieux et intimidant. Lorsqu'il combat des ennemis ou lorsqu'il
+          utilise ses pouvoirs, les yeux de Jin-Woo brillent d'un bleu pâle.
+          Dans certains cas, une aura inquiétante suinte de Jin-Woo aux côtés de
+          la foudre sombre. Cela s'affiche dans les moments où Jin-Woo est
+          enragé, comme lorsqu'il a tué les lézards pendant l' arc des donjons
+          et des lézards . Jin-Woo commente notamment que sa croissance rapide
+          et ses changements physiques peuvent être dus aux effets du système à
+          mesure qu'il augmente ses statistiques.
+        </p>
+        <h2 id="b">Personalité</h2>
+        <p>
+          Avant son réveil , Jin-Woo était un jeune homme gentil, nerveux et
+          très timide. Malgré cela, il respectait ses camarades chasseurs et
+          était assez courageux pour continuer à se battre dans les donjons pour
+          gagner de l'argent pour prendre soin de sa sœur et pour payer les
+          factures médicales de sa mère; en dépit d'être le chasseur le plus
+          faible qui ait existé.
+        </p>
+        <p>
+          Jin-Woo se révèle également être une personne intelligente, capable de
+          rester calme sous la pression alors qu'il découvrait les indices pour
+          combattre le patron du Double Dungeon alors que la plupart des
+          chasseurs expérimentés de leur groupe paniquaient et étaient tués par
+          les statues lorsque ils essayaient de s'échapper. Cela le rend
+          également légèrement prudent car il hésitait à révéler sa force
+          croissante jusqu'à ce qu'il soit sûr de pouvoir protéger sa famille.
+        </p>
+        <p>
+          Après son réveil, il s'efforce de devenir un chasseur plus fort
+          qu'avant. Ses seuls objectifs étaient d'accomplir ses missions
+          quotidiennes et de progresser via l'exploration de donjons. Il semble
+          ne pas hésiter dans ses actions et blesserait et nuirait de manière
+          significative à quiconque se menace ou à ceux dont il se soucie
+          vraiment. Malgré cela, il ne veut pas attirer l'ombre de chasseurs /
+          civils innocents. Il n'a fait que des cas rares, comme l'extraction de
+          l' ombre de Min Byung-Gyu pour soigner Cha Hae-In. Il a un grand amour
+          pour ses proches, y compris sa famille et ses amis proches tels que sa
+          sœur Sung Jin-Ah à qui il a attaché de puissants soldats de l'ombre
+          des hauts orques pour la protéger lors de situations dangereuses
+          telles que la porte orque qui s'est ouverte à l'intérieur de l'école.
+          assisté.
+        </p>
+
+        <p>
+          Il est facilement enragé si quelqu'un qui lui est cher était blessé ou
+          menacé et faisait de grands efforts pour les protéger, comme on l'a vu
+          lorsque Hwang Dong-Su a kidnappé Yoo Jin-Ho où il a combattu toute la
+          Guilde des Scavenger seul pour le bien de son cher ami. Bien que
+          Jin-Woo se soit montré indifférent à l'argent, à la richesse et même
+          aux désirs humains, il a admis avoir jeté par inadvertance un coup
+          d'œil à la forme nue de Hae-In après avoir utilisé imprudemment le ``
+          partage sensoriel '' à travers le soldat de l'ombre inséré dans son
+          ombre.
+        </p>
+        <p>
+          Jin-Woo a affiché une augmentation substantielle de sa confiance après
+          avoir reçu le mystérieux «Double Awakening». Il est devenu puissant
+          tout en conservant une personnalité détendue. Jin-Woo ne faiblit pas
+          en cas de crise ou de menace et est rarement en colère. Il a montré
+          moins d'émotion, ce qui peut être causé par l'utilisation du système
+          comme mentionné par Jin-Woo lui-même lorsqu'il déclare: «Une de mes
+          émotions est morte», faisant référence à la possibilité que la
+          puissance du système l'affecte dans certains façon. Malgré cela, il
+          remet rarement en question le système et ses origines inconnues, qui
+          seront expliquées plus tard dans l'histoire. Il a également tendance à
+          sourire lorsqu'il fait face à une situation difficile et conserve
+          toujours la confiance de lui-même en sortant vainqueur à la fin.
+        </p>
+        <p>
+          Même après avoir atteint une grande puissance, Jin-Woo ne maintient
+          pas un comportement agressif et trop confiant envers les autres. Il
+          est décrit par d'autres comme étant facile à parler et beaucoup plus
+          acceptant certaines situations auxquelles il peut être confronté. Dans
+          un cas, deux membres de l'American Hunter Association ont pointé des
+          armes sur Jin-Woo à la suite d'un malentendu. Cependant, Jin-Woo n'a
+          pas utilisé ses pouvoirs mais a plutôt calmement diffusé la situation.
+        </p>
+        <p>
+          Jin-Woo définit systématiquement ses actions par un ensemble de codes
+          moraux. Il refuse de ressusciter des humains innocents pour servir de
+          soldats de l'ombre et fait tout pour protéger ceux qui sont plus
+          faibles que lui. Jin-Woo a, à quelques reprises, enfreint de tels
+          dirigeants, comme dans le cas de Min Byung-Gyu dont les capacités
+          étaient nécessaires pour sauver le mourant Cha Hae-In.
+        </p>
+        <h2 id="c">contexte</h2>
+        <p>
+          Jin-Woo est l'aîné de sa famille. Après que son père, Il-Hwan ait
+          disparu à l'intérieur d'une porte inconnue, Jin-Woo a pris toutes les
+          responsabilités de sa sœur, Jin-Ah et Kyung-He, sa mère. Jin-Woo et sa
+          sœur vivent dans la même maison depuis qu'ils sont enfants. Leur mère
+          a été victime de la maladie du sommeil éternel, laissant Jin-Woo et
+          Jin-Ah seuls pendant les 4 dernières années depuis qu'elle est tombée
+          dans un état comateux après avoir contracté la maladie.
+        </p>
+        <p>
+          Il a travaillé comme E-Classé Hunter sous l' Association des chasseurs
+          coréens pendant 4 ans, la lutte contre une lutte quotidienne de la vie
+          et la mort comme il a entrepris des voyages dans des donjons classés
+          bas comme le plus faible chasseur E-Rank simplement pour soutenir sa
+          famille dans une telle moment difficile. Il a persévéré à travers ces
+          cachots pour payer les factures médicales de sa mère et les frais de
+          scolarité de sa jeune sœur alors qu'il tentait de combler le fossé
+          laissé par leur père disparu qui a disparu à l'intérieur d'une porte
+          tout en essayant de protéger ses collègues.
+        </p>
+        <p>
+          <li>
+            "Ce n'est pas une coïncidence ou une bonne volonté. C'est ce dont le
+            système a besoin. Un Sung Jin-Woo fort."
+          </li>
+          <li>
+            "Je protégerai ma famille, même si cela signifie que chaque Chasseur
+            du monde devient mon ennemi."
+          </li>
+          <li>"C'est un fait que je serai trahi parce que je suis faible."</li>
+          <li>
+            " C'est la deuxième fois que tu fais ça ... en touchant mon épaule.
+            Mais, la seule différence par rapport à la dernière fois est que ce
+            n'est pas une commande mais une demande sérieuse. " - Jin-Woo à Baek
+            Yoon-Ho
+          </li>
+          <li>
+            "Arrêtez de protéger le siège de quelqu'un qui est déjà parti et
+            essayez de me protéger, qui est devant vous"
+          </li>
+          <li>
+            "Si vous êtes des chasseurs, alors essayez de chasser ... De toute
+            façon, les chasseurs étaient faits pour chasser"
+          </li>
+        </p>
+        <h2 id="d">citations</h2>
+        <p>
+          vitae hendrerit volutpat. Aliquam erat volutpat. Vivamus tincidunt
+          turpis id sapien ullamcorper, ut interdum elit rhoncus. Mauris finibus
+          ligula ac dui rutrum, et tincidunt libero fringilla. Donec elementum
+          odio ac enim facilisis, in bibendum enim scelerisque. Quisque nec
+          suscipit arcu. Maecenas pretium vulputate varius. Curabitur quam
+          turpis, tempus vitae sagittis sed, scelerisque mollis diam. Phasellus
+          accumsan sed ex ut dictum. Aliquam commodo feugiat purus sed
+          porttitor. Duis cursus felis condimentum, vulputate ante vitae, mollis
+          quam.
+        </p>
+
+        <h2 id="e">trivia</h2>
+        <p>
+          Attention! Ce qui suit est de nouveaux spoilers légers qui n'ont pas
+          été couverts dans le Solo Leveling Manhwa
+        </p>
+        <li>
+          Même si Jin-Woo et Ashborn ont fusionné, malgré qu'Ashborn utilise une
+          épée longue, Jin-Woo a été déterminé à utiliser une épée courte parce
+          que son père, Il-Hwan utilise une épée courte.
+        </li>
+        <li>
+          Jin-Woo a été réprimandé une fois parce qu'il jouait avec l'épée
+          courte de son père.
+        </li>
+        <li>
+          Jin-Woo a accidentellement laissé une cicatrice sur la nuque de sa
+          mère après avoir essayé de se laver les cheveux avec de l'eau
+          bouillante.
+        </li>
+      </div>
+      <div id="somm"> 
+        <h1>sommaire</h1>
+        <li> <a href="#a">apparence</a> </li>
+        <li> <a href="#b">personnalité</a> </li>
+        <li> <a href="#c">contexte</a> </li>
+        <li> <a href="#d">citations</a> </li>
+        <li> <a href="#e">trivia</a> </li>
+      </div>
+    </section>
+
+    <section  >
+     
+      <div >
+        <div id="caract" class="corp" >
+           <div class="desc">
+           <h1>Sung Jin-Woo</h1>
+           <h3>Hangul</h3>
+           <p>성진우</p>
+           <h3>debut</h3>
+           <p>Chapter 1NV, Chapter 1WB</p>
+           <h3>epithets</h3>
+
+           <li>E-Rank Hunter</li>
+           <li>World's Weakest Hunter</li>
+           <li>S-Rank Hunter</li>
+           <li>South Korea's 10th S-Rank</li>
+           <li>National Level Hunter</li>
+           <li>2nd Rate Asian Hunter</li>
+          <li>Shadow Monarch</li>
+         </div>
+          <img id="imgcaract" src="Jin-Woo_Profile.png" alt=""  />
+        </div>
+
+       
+      </div>
+      
+      
+    </section>
+  </div>
+
+    <footer></footer>
+  </body>
+</html>
